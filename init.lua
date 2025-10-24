515774c1fb14f2cd00f03e1eb6a6c7df565ca0ee
Noah Gershenson <noah.gershenson@powerhrg.com>
2025-10-24 17:20:06 -0400

    Install vim be good training games

--- init.lua
+++ init.lua
@@ -1,2 +1,6 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
