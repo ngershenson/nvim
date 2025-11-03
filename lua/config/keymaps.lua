b77037f4a336610914d72a10a10a79d7ad305bca
Noah Gershenson <noah.gershenson@powerhrg.com>
2025-11-03 13:09:46 -0500

    Use jj to esc insert mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,4 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "<leader>qL", Snacks.dashboard.open, { desc = "Open Snacks Dashboard" })
+vim.keymap.set("i", "jj", "<Esc>", { desc = "Exit insert mode" })
